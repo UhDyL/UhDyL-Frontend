51d8993b23228fdfc97b2e17d831d0d014559345
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-06 23:16:35 +0900

    [Feat] #6 - imgMap 파일 생성하여 정리

---
+++ constants/imgMap.ts
@@ -0,0 +1,9 @@
+export const imgMap = {
+  fruit: require('../assets/images/consumermain/fruit_img.png'),
+  vegetable: require('../assets/images/consumermain/vegetable_img.png'),
+  rootVegetable: require('../assets/images/consumermain/root_vegetable_img.png'),
+  grain: require('../assets/images/consumermain/grain_img.png'),
+  forJuiceOrJam: require('../assets/images/consumermain/for_juice_jam_img.png'),
+  forSideDish: require('../assets/images/consumermain/for_sidedishes_img.png'),
+  forGift: require('../assets/images/consumermain/for_gifts_img.png'),
+};
