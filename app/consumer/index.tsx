dfbe963f20d1e717623c9c9808102a5360b98210
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-06 18:12:08 +0900

    [Feat] #4 - 닉네임 입력 페이지 구현

---
+++ app/consumer/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './ConsumerMainScreen.styled';
+import { Text } from 'react-native';
+
+export default function ConsumerMainScreen() {
+  return (
+    <Container>
+      <Text>hi</Text>
+    </Container>
+  );
+}
