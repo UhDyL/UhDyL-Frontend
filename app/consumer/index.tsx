39e1e03857fc70dfc178326aab2eb7cef7006fd1
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-06 18:16:55 +0900

    [Feat] #4 - 소비자 화면 Layout 설정

--- app/consumer/index.tsx
+++ app/consumer/index.tsx
@@ -1,9 +1,11 @@
 import { Container } from './ConsumerMainScreen.styled';
+import { Stack } from 'expo-router';
 import { Text } from 'react-native';
 
 export default function ConsumerMainScreen() {
   return (
     <Container>
+      <Stack.Screen options={{ title: '어글리 딜리셔스' }} />
       <Text>hi</Text>
     </Container>
   );
