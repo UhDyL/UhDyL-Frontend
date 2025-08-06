4d061e055b530dabe5e467b120ddc0885b80eb69
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-06 12:30:07 +0900

    [Feat] #4 - 닉네임 입력 및 검증 컴포넌트 구현

---
+++ components/nicknameInput/NicknameInput.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 80%;
+`;
+
+export const NicknameInputArea = styled.TextInput`
+  padding: 10px;
+  border-bottom-width: 1px;
+  border-bottom-color: #d9d9d9;
+`;
+
+export const ErrorText = styled.Text`
+  color: red;
+  font-size: 14px;
+  margin-top: 5px;
+`;
