059e234d47877ac65a43a8852d5a19bb3415c355
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-07 17:27:24 +0900

    [Fix] #7 - CategoryItemWrapper 여백 문제 해결

--- components/consumer/categoryItem/categoryItemWrapper.styled.ts
+++ components/consumer/categoryItem/categoryItemWrapper.styled.ts
@@ -1,21 +1,19 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled.View`
   flex: 1;
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 `;
 
 export const ItemsContainer = styled.View`
-  flex: 1;
   width: 80%;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: flex-start;
 `;
