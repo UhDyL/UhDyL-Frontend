6b01dad4da81c602b2335198d6cbd243cd1eb849
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-07 15:39:27 +0900

    [Feat] #6 - 프로필 기본 페이지 구상

---
+++ app/profile/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Container } from './profile.styled';
+
+export default function ProfileScreen() {
+  return (
+    <Container>
+      <Text>Profile Screen</Text>
+    </Container>
+  );
+}
