a53515b34cf12d40e8637eff46f8641cb31aee2d
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-08 22:28:20 +0900

    [Style] #8 - 레이아웃 미세조정

--- components/seller/summaryBox/infoItemBar/infoItemBar.styled.ts
+++ components/seller/summaryBox/infoItemBar/infoItemBar.styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 200px;
+  margin-right: 10px;
   padding: 4px;
   display: flex;
   flex-direction: row;
--- components/seller/topHeader/topHeader.styled.ts
+++ components/seller/topHeader/topHeader.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
-  height: 30px;
+  height: 60px;
   display: flex;
   flex-direction: row;
   justify-content: center;
