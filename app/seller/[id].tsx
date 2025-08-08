6ff81fc5b28911baf4dc6df07a92e9c7dc17876c
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-08 23:24:28 +0900

    [Feat] #8 - 판매자 상점 리뷰 화면 UI 퍼블리싱

--- app/seller/[id].tsx
+++ app/seller/[id].tsx
@@ -1,20 +1,60 @@
 import { Container, SummaryArea } from './seller.styled';
 
+import { ReviewItemProps } from '@/components/seller/reviewsBox/reviewItem/ReviewItem';
+import ReviewsBox from '@/components/seller/reviewsBox/ReviewsBox';
+import { ScrollView } from 'react-native';
 import SummaryBox from '@/components/seller/summaryBox/SummaryBox';
+import TabBar from '@/components/common/tabBar/TabBar';
 import TopHeader from '@/components/seller/topHeader/TopHeader';
 import { useLocalSearchParams } from 'expo-router';
-import { Text } from 'react-native';
+
+const dummyReviews: ReviewItemProps[] = [
+  {
+    name: '토마토',
+    rating: 4.5,
+    createdAt: new Date('2024-07-20'),
+    buyer: 'cheolsu123',
+    content:
+      '상품이 전반적으로 만족스러웠어요. 포장도 깔끔하고 배송도 빨랐습니다.',
+  },
+  {
+    name: '당근',
+    rating: 5.0,
+    createdAt: new Date('2024-08-01'),
+    buyer: 'younghee88',
+    content: '진짜 너무 좋아요! 친구한테도 추천했어요 :)',
+  },
+  {
+    name: '양파즙',
+    rating: 3.0,
+    createdAt: new Date('2024-08-04'),
+    buyer: 'minjun_park',
+    content: '보통이에요. 생각보다 크기가 작아서 아쉬웠습니다.',
+  },
+  {
+    name: '김치국',
+    rating: 4.0,
+    createdAt: new Date('2024-08-06'),
+    buyer: 'seoyeonchoi',
+    content: '가성비는 좋아요. 약간의 흠집이 있었지만 사용할 만합니다.',
+  },
+];
 
 export default function SellerScreen() {
   const { id } = useLocalSearchParams();
 
   return (
-    <Container>
-      <TopHeader />
-      <SummaryArea>
-        <SummaryBox imgUrl='' />
-      </SummaryArea>
-      <Text>{id}</Text>
-    </Container>
+    <>
+      <ScrollView>
+        <Container>
+          <TopHeader />
+          <SummaryArea>
+            <SummaryBox imgUrl='' />
+          </SummaryArea>
+          <ReviewsBox data={dummyReviews} />
+        </Container>
+      </ScrollView>
+      <TabBar />
+    </>
   );
 }
