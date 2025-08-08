4200a07628cc4bc848293366af78d3fc50ed51eb
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-08 16:46:25 +0900

    [Feat] #8 - 좋아요 버튼 클릭 시 스타일 설정

--- components/itemDetail/bottomBar/BottomBar.tsx
+++ components/itemDetail/bottomBar/BottomBar.tsx
@@ -6,12 +6,15 @@ import {
 } from './bottomBar.styled';
 
 import { Heart } from 'lucide-react-native';
+import { useState } from 'react';
 
 export default function BottomBar() {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
   return (
     <Container>
-      <LikeBtn>
-        <Heart />
+      <LikeBtn onPress={() => setIsLiked(!isLiked)}>
+        <Heart color={isLiked ? '#30DB5B' : '#d9d9d9'} />
       </LikeBtn>
       <ChattingBtn>
         <TextWrapper>채팅하기</TextWrapper>
