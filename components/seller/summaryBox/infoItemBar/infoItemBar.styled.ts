01a002257046e8cd17bbe7acb5d7d29529830c95
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-08 22:15:14 +0900

    [Feat] #8 - 정보 InfoItemBar 컴포넌트 생성

---
+++ components/seller/summaryBox/infoItemBar/infoItemBar.styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 200px;
+  padding: 4px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 15px;
+`;
+
+export const LeftSection = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const LabelText = styled.Text`
+  font-weight: 700;
+`;
+
+export const ValueBox = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const ValueText = styled.Text`
+  font-weight: 700;
+  padding-right: 10px;
+`;
