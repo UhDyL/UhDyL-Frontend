1c5f23dc1beefff232c60a39b3329b71e714b1d2
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-09 11:55:40 +0900

    [Fix] #10 - TabBar 현재 상태 props로 입력받도록 수정

--- components/common/tabBar/tabBar.styled.ts
+++ components/common/tabBar/tabBar.styled.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  height: 100px;
+  width: 100%;
+  height: 110px;
   background-color: #fff;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
   padding: 10px;
+  padding-bottom: 20px;
+  position: absolute;
+  bottom: 0;
+  border-top-width: 1px;
+  border-top-color: #0000001a;
   padding-bottom: 20px;
 `;
 
