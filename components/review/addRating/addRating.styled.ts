fd9ca4cff3c8c37b58bb43cd06f51acd7a2e0c2d
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-09 17:57:08 +0900

    [Feat] #10 - 별점 선택 컴포넌트 생성

---
+++ components/review/addRating/addRating.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TitleText = styled.Text`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const DesctriptionText = styled.Text`
+  font-size: 12px;
+`;
