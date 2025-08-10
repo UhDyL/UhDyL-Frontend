59639cf8d59ef125bdcacd9ab9acfcaabfba355a
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-11 08:44:20 +0900

    [Feat] #21 - user정보 전역으로 저장 zustand 설정

---
+++ store/userStore.ts
@@ -0,0 +1,27 @@
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+
+type UserState = {
+  userType: 'farmer' | 'seller' | null;
+  name: string | null;
+  setUserType: (t: UserState['userType']) => void;
+  setName: (n: string | null) => void;
+};
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      userType: null,
+      name: null,
+      setUserType: (t) => set({ userType: t }),
+      setName: (n) => set({ name: n }),
+    }),
+    {
+      name: 'user-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (s) => ({ userType: s.userType }),
+    }
+  )
+);
