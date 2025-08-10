0133fb719eed95254036cb9b0f373544f043e1ae
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-10 14:53:12 +0900

    [Feat] #11 - Button 컴포넌트 분리

---
+++ components/common/button/button.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  width: 80%;
+  height: 60px;
+  background-color: #30db5b;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: 800;
+`;
