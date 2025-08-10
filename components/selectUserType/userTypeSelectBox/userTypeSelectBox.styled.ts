8182fd8c5b48bfd76b868ebfa53dc76781841f99
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-10 14:46:33 +0900

    [Feat] #11 - userType 선택하는 컴포넌트 구현

---
+++ components/selectUserType/userTypeSelectBox/userTypeSelectBox.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const SelectUserTypeBox = styled.TouchableOpacity`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
