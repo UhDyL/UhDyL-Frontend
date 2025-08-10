9e429a107a48c5ad81dd781a7fd42ea522b4177b
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-10 17:59:29 +0900

    [Feat] #17 - 농부 메인 페이지 UI 퍼블리싱

--- app/farmer/FarmerMainScreen.styled.ts
+++ app/farmer/FarmerMainScreen.styled.ts
@@ -1,4 +1,16 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)``;
+export const Container = styled(SafeAreaView).attrs({
+  edges: ['bottom', 'left', 'right'],
+})`
+  padding-top: 40px;
+  width: 100%;
+  flex: 1;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 40px;
+`;
