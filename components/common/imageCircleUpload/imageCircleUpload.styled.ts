b2940336a58af2db4144ec34904dbe4f738ce0a2
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-10 13:58:33 +0900

    [Feat] #11 - 이미지 업로드 컴포넌트 분리

---
+++ components/common/imageCircleUpload/imageCircleUpload.styled.ts
@@ -0,0 +1,31 @@
+import { Image } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const ImageBox = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledImage = styled(Image)`
+  width: 140px;
+  height: 140px;
+  border-radius: 70px;
+  overflow: hidden;
+`;
+
+export const CameraBox = styled.TouchableOpacity`
+  width: 30px;
+  height: 30px;
+  padding: 10px;
+  border-radius: 50%;
+  background-color: #30db5b;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
