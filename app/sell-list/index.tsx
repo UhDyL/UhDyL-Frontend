d7d8ce6fdf7fdcf6d666850d920d0593680fcd0f
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-12 18:07:10 +0900

    [Fix] #20 - 테스트 텍스트 제거

--- app/sell-list/index.tsx
+++ app/sell-list/index.tsx
@@ -1,7 +1,6 @@
 import BackAndTitle from '@/components/common/backAndTitle/BackAndTitle';
 import SellListBox from '@/components/farmer/sell-list/sellListBox/SellListBox';
 import TopOptionBar from '@/components/farmer/sell-list/topOptionBar/TopOptionBar';
-import { Text } from 'react-native';
 import { Container } from './sellList.styled';
 
 export default function SellList() {
@@ -10,7 +9,6 @@ export default function SellList() {
       <BackAndTitle title='판매 목록' />
       <TopOptionBar />
       <SellListBox />
-      <Text>HI</Text>
     </Container>
   );
 }
