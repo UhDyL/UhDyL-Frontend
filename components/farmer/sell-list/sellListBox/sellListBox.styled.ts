f8c9b28a4790b5f31a2d15a76b130c879d1c9c48
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-12 15:01:58 +0900

    [Feat] #20 - 판매 목록 box 컴포넌트 생성

---
+++ components/farmer/sell-list/sellListBox/sellListBox.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
