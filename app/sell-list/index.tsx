e52502661ac72d961f34ee0f076269184b9d90ec
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-12 15:00:57 +0900

    [Feat] #20 - 판매 목록 페이지 UI 퍼블리싱

--- app/sell-list/index.tsx
+++ app/sell-list/index.tsx
@@ -1,4 +1,6 @@
 import BackAndTitle from '@/components/common/backAndTitle/BackAndTitle';
+import SellListBox from '@/components/farmer/sell-list/sellListBox/SellListBox';
+import TopOptionBar from '@/components/farmer/sell-list/topOptionBar/TopOptionBar';
 import { Text } from 'react-native';
 import { Container } from './sellList.styled';
 
@@ -6,6 +8,8 @@ export default function SellList() {
   return (
     <Container>
       <BackAndTitle title='판매 목록' />
+      <TopOptionBar />
+      <SellListBox />
       <Text>HI</Text>
     </Container>
   );
--- app/sell-list/sellList.styled.ts
+++ app/sell-list/sellList.styled.ts
@@ -1,4 +1,12 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)``;
+export const Container = styled(SafeAreaView)`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #fff;
+`;
