d7353b506e9afc0f7cdfd1e2e1c4fdfb24c2eddf
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-08-17 18:30:20 +0900

    [Fix] #28 - userInfo GET 훅 경로 변경

--- hooks/query/useGetUserInfo.ts
+++ hooks/query/useGetUserInfo.ts
@@ -1,9 +1,11 @@
 import { getUserInfo, type UserInfoResponseDto } from '@/api/user.api';
 import { hydrateUserToStore } from '@/store/hydrateUser';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
 export function useGetUserInfo() {
+  const qc = useQueryClient();
+
   const query = useQuery<UserInfoResponseDto>({
     queryKey: ['user', 'me'],
     queryFn: getUserInfo,
@@ -11,10 +13,9 @@ export function useGetUserInfo() {
   });
 
   useEffect(() => {
-    if (query.data) {
-      hydrateUserToStore(query.data);
-    }
-  }, [query.data]);
+    const cached = qc.getQueryData<UserInfoResponseDto>(['user', 'me']);
+    if (cached) hydrateUserToStore(cached);
+  }, [qc, query.dataUpdatedAt]);
 
   return query;
 }
