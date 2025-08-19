856db00e06bde774fe4df0fc89cefc121fbb2971
minwoo1119 <minwoo1119@naver.com>
2025-08-19 17:47:57 +0900

    [Fix] #29 - 불필요한 코드 수정

--- hooks/mutation/useToggleZzim.ts
+++ hooks/mutation/useToggleZzim.ts
@@ -1,36 +1,9 @@
-import type { GetProductDetailResponseDto } from '@/api/product.api';
 import { PostZzimResponseDto, toggleZzim } from '@/api/zzim.api';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
-export const useToggleZzim = (
-  productId: number,
-  options?: Parameters<
-    typeof useMutation<PostZzimResponseDto, Error, void, unknown>
-  >[0]
-) => {
-  const qc = useQueryClient();
-
-  return useMutation<PostZzimResponseDto, Error, void, unknown>({
-    mutationKey: ['useToggleZzim'],
+export const useToggleZzim = (productId: number) => {
+  return useMutation<PostZzimResponseDto>({
+    mutationKey: ['useToggleZzim', productId],
     mutationFn: () => toggleZzim(productId),
-    onSuccess: (data) => {
-      qc.setQueryData<GetProductDetailResponseDto>(
-        ['useGetProductDetail', productId],
-        (old) => (old ? { ...old, isZzim: data.isZzim } : old)
-      );
-
-      qc.setQueryData<PostZzimResponseDto[]>(['useGetZzimList'], (old) => {
-        if (!old) return old;
-        return data.isZzim
-          ? [...old, data]
-          : old.filter((z) => z.zzim.id !== data.zzim.id);
-      });
-
-      qc.invalidateQueries({ queryKey: ['useGetProductDetail', productId] });
-      qc.invalidateQueries({ queryKey: ['useGetZzimList'] });
-
-      options?.onSuccess?.(data, undefined as any, undefined as any);
-    },
-    ...options,
   });
 };
