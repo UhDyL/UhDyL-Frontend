83de56fdcb3ad93013042f3540f6d2d138ed783e
minwoo1119 <minwoo1119@naver.com>
2025-08-19 20:58:00 +0900

    [Style] #29 - 프로필 정보 페이지 SafeArea 수정

--- app/profile/profile.styled.ts
+++ app/profile/profile.styled.ts
@@ -1,7 +1,9 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled(SafeAreaView).attrs({
+  edges: ['left', 'right', 'top'],
+})`
   flex: 1;
   display: flex;
   flex-direction: column;
