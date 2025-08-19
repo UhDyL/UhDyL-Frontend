d1c7c50b5bc3a175a60e427525a65d5c7d7c98ca
minwoo1119 <minwoo1119@naver.com>
2025-08-19 10:10:46 +0900

    [Feat] #29 - 상품 상세조회 api 생성

--- api/product.api.ts
+++ api/product.api.ts
@@ -24,3 +24,29 @@ export const getProductsList = async () => {
     throw err;
   }
 };
+
+export interface GetProductDetailResponseDto {
+  id: number;
+  name: string;
+  title: string;
+  price: number;
+  description: string;
+  sellerName: string;
+  sellerPicture: string;
+  sellerRating: number;
+  images: string[];
+  isCompleted: boolean;
+}
+
+export const getProductDetail = async (productId: number) => {
+  try {
+    const response = await fetcher.get<{
+      success: boolean;
+      data: GetProductDetailResponseDto;
+    }>(`/product/${productId}`);
+    return response.data.data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
