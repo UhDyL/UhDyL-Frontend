f1dcb065cc6158ffcefc22c519bd0a6f10e9197f
minwoo1119 <minwoo1119@naver.com>
2025-08-21 22:13:55 +0900

    [Fix] #30 - useMutation으로 변경

--- hooks/query/useGetMyFieldLocation.ts
+++
@@ -1,9 +0,0 @@
-import { getMyFieldLocation } from '@/api/user.api';
-import { useQuery } from '@tanstack/react-query';
-
-export const useGetMyFieldLocation = () => {
-  return useQuery({
-    queryKey: ['getMyFieldLocation'],
-    queryFn: () => getMyFieldLocation(),
-  });
-};
