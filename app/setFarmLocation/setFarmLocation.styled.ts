efe176d14833b0205e8839e9330a67c1f56eb916
minwoo1119 <minwoo1119@naver.com>
2025-08-22 11:06:57 +0900

    [Style] #30 - 위치 설정 컴포넌트 수정

--- app/setFarmLocation/setFarmLocation.styled.ts
+++ app/setFarmLocation/setFarmLocation.styled.ts
@@ -1,8 +1,26 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled(SafeAreaView).attrs({
+  edges: ['right', 'left'],
+})`
   flex: 1;
   width: 100%;
   background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+`;
+
+export const ButtonWrapper = styled.View`
+  position: absolute;
+  bottom: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
 `;
