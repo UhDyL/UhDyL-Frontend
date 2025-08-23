6e699f87fb2320a4132eff7af1b2f8f265e54f38
minwoo1119 <minwoo1119@naver.com>
2025-08-23 13:19:30 +0900

    [Feat] #30 - 첫번째 페이지 상품 등록 api 연결

--- app/new-item/step1.tsx
+++ app/new-item/step1.tsx
@@ -1,37 +1,58 @@
-import { Container, JustBox } from './newItem.styled';
+import {
+  Container,
+  JustBox,
+  SubText,
+  TextBox,
+  TitleText,
+} from './newItem.styled';
 
+import AddPhotos from '@/components/review/addPhotos/AddPhotos';
 import Button from '@/components/common/button/Button';
-import InputForm from '@/components/common/inputForm/InputForm';
 import LevelAndGoHome from '@/components/farmer/levelAndGoHome/LevelAndGoHome';
-import SelectKeywordBox from '@/components/farmer/seleectKeywordBox/SelectKeywordBox';
-import { dummyKeywords } from '@/mocks/dummyKeywords';
+import { useFormStore } from '@/store/useFormStore';
+import { usePostProductImage } from '@/hooks/mutation/usePostProductImage';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
 
-export default function NewItemStepOneScreen() {
-  const [breed, setBreed] = useState<string>('');
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+export default function NewItemStepTwoScreen() {
+  const [imagesUrl, setImagesUrl] = useState<string[]>([]);
   const router = useRouter();
+  const { formData, setFormData } = useFormStore();
+
+  const { mutateAsync: uploadImage } = usePostProductImage();
+
+  const handleUploadImages = async () => {
+    try {
+      const results = await Promise.all(
+        imagesUrl.map((image) => uploadImage(image))
+      );
+
+      const uploadedUrls = results.map((res) => res.imageUrl);
+      setImagesUrl(uploadedUrls);
+
+      setFormData({ images: uploadedUrls });
+
+      router.push('/new-item/step2');
+    } catch (error) {
+      console.error('이미지 업로드 실패:', error);
+    }
+  };
 
   return (
     <Container>
       <LevelAndGoHome level={1} />
-      <InputForm
-        label='품종을 입력해주세요.'
-        placeholder='예) 사과'
-        value={breed}
-        setValue={setBreed}
-      />
-      <SelectKeywordBox
-        keywords={dummyKeywords}
-        selected={selectedOptions}
-        setSelected={setSelectedOptions}
-      />
+      <TextBox>
+        <TitleText>사진</TitleText>
+        <SubText>최대 6장까지 첨부할 수 있어요</SubText>
+      </TextBox>
+      <AddPhotos imagesUrl={imagesUrl} setImagesUrl={setImagesUrl} />
       <JustBox>
         <Button
           size='full'
           text='다음'
-          onClick={() => router.push('/new-item/step2')}
+          onClick={() => {
+            handleUploadImages();
+          }}
         />
       </JustBox>
     </Container>
