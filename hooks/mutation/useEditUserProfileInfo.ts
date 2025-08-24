82c62476f2f7ef656d2d9337358ac4004ef4e6f8
minwoo1119 <minwoo1119@naver.com>
2025-08-24 21:35:48 +0900

    [Fix] #33 - 프로필 수정 api 수정된 것만 전송하도록 수정

--- hooks/mutation/useEditUserProfileInfo.ts
+++ hooks/mutation/useEditUserProfileInfo.ts
@@ -2,26 +2,15 @@ import {
   EditUserProfileInfo,
   EditUserProfileInfoRequestDto,
 } from '@/api/user.api';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { useMutation } from '@tanstack/react-query';
 
 export const useEditUserProfileInfo = () => {
-  const qc = useQueryClient();
-
   return {
     ...useMutation({
-      mutationFn: EditUserProfileInfo,
+      mutationFn: (data: EditUserProfileInfoRequestDto) =>
+        EditUserProfileInfo(data),
       mutationKey: ['editUserProfileInfo'],
-      onSuccess: (_res, vars) => {
-        qc.setQueryData<EditUserProfileInfoRequestDto>(
-          ['editUserProfileInfo'],
-          (prev) => (prev ? { ...prev, ...vars } : prev)
-        );
-
-        qc.invalidateQueries({
-          queryKey: ['editUserProfileInfo'],
-          refetchType: 'active',
-        });
-      },
     }),
   };
 };
