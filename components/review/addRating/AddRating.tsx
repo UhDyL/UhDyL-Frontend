97b434116dfdde69a63fe560ad1baca51361497d
minwoo1119 <minwoo1119@naver.com>
2025-09-24 21:47:49 +0900

    [Style] #40 - 별점 수정
    - 0.5단위로 수정
    - 커스텀 이미지화

--- components/review/addRating/AddRating.tsx
+++ components/review/addRating/AddRating.tsx
@@ -1,6 +1,7 @@
 import { Container, DesctriptionText, TitleText } from './addRating.styled';
 
-import { Rating } from 'react-native-ratings';
+import { Image } from 'react-native';
+import StarRating from 'react-native-star-rating-widget';
 
 type Props = {
   rating: number;
@@ -11,10 +12,25 @@ export default function AddRating({ rating, setRating }: Props) {
   return (
     <Container>
       <TitleText>별점을 매겨 보세요</TitleText>
-      <Rating
-        onFinishRating={(rating: number) => setRating(rating)}
-        startingValue={rating}
-        style={{ margin: 20 }}
+      <StarRating
+        rating={rating}
+        onChange={(val: number) => setRating(val)}
+        enableHalfStar
+        starStyle={{ marginHorizontal: 2 }}
+        starSize={40}
+        StarIconComponent={({ type, size }) => (
+          <Image
+            source={
+              type === 'half'
+                ? require('../../../assets/images/review/half-star-image.png')
+                : type === 'full'
+                ? require('../../../assets/images/review/full-star-image.png')
+                : require('../../../assets/images/review/empty-star-image.png')
+            }
+            style={{ width: size, height: size }}
+            resizeMode='contain'
+          />
+        )}
       />
       <DesctriptionText>별을 선택하세요</DesctriptionText>
     </Container>
